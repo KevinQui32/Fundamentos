9f168163af314646c9d8256bf648c9af2c07165c
KevinQui32 <kevin.caicedo.quinonez@utelvt.edu.ec>
2022-09-24 00:12:49 -0500

    programa de suma.cpp

--- suma.cpp
+++ suma.cpp
@@ -1,2 +1,12 @@
-//
-Hola mundo
+#include<iostream>
+using namespace std;
+int main()
+{
+	float k,c,q;
+	cout<<"ingrese primer numero k=";cin>>k;
+	cout<<"ingrese el segundo numero c=";cin>>c;
+	q=k+c;
+	cout<<"El resultado es "<<k<<" + "<<c<<"="<<q<<endl;
+	return 0;
+}
+//gracias ;
