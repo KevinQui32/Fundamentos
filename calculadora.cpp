5d54282e7d22128477ad19b195b35e945d6b4126
KevinQui32 <kevin.caicedo.quinonez@utelvt.edu.ec>
2022-09-24 10:47:13 -0500

    calculadora modificada

---
+++ calculadora.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float k,c,s,r,d,m;
+	cout<<"ingrese el valor de k="; cin>>k;
+	cout<<"ingrese en valor de c="; cin>>c;
+	s=k+c;
+	r=k-c;
+	m=k*c;
+	d=k/c;
+	cout<<"el resultado de la suma es"<<k<<"+"<<c<<"="<<s<<endl;
+	cout<<"el resultado de la resta es"<<k<<"-"<<c<<"="<<r<<endl;
+	cout<<"el resultado de la multiplicacion es"<<k<<"×"<<c<<"="<<m<<endl;
+	cout<<"el resultado de la divicion es"<<k<<"/"<<c<<"="<<d<<endl;
+	return 0;
+}
+//gracias ;
+	
+	
