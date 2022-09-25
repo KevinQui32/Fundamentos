17a7778b166586546bdd6ae5be1d7c80f6e541ab
KevinQui32 <kevin.caicedo.quinonez@utelvt.edu.ec>
2022-09-24 21:51:59 -0500

    programa velocimetro.cpp

---
+++ velocimetro.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float k,c,q;
+	cout<<"ingrse la distancia (en km) k=";cin>>k;
+	cout<<"ingrese el tiempo (en segundo) c=";cin>>c;
+	q=k/c;
+	cout<<"lavelocidad calculada es de q="<<q<<endl<<endl;
+	return 0;
+}
+//gracias;
