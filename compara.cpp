cd809e8676fe97237d625383bca92dea346a596d
KevinQui32 <kevin.caicedo.quinonez@utelvt.edu.ec>
2022-09-24 22:07:12 -0500

    programa compara.cpp

---
+++ compara.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float k,c;
+	cout<<"ingrse k c:"; cin>>k>>c;
+	if(k>c)
+	{
+	   cout<<k<<"es mayor k "<<c<<endl;
+
+	}else{
+	   
+	   cout<<c<<"es mayor k "<<k<<endl;
+
+	}
+
+	return 0;
+
+
+
+}
+//gracias ;
